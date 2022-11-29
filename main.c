9d6ecb370a0edd6c16de991b169f43413ce055f8
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2022-11-29 15:00:08 +0100

    added build script and a bunch of c cmpatible functions

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
+#include <stddef.h>
 #include <stdio.h>
+
+struct find_match
+{
+    int    found;
+    size_t start;
+    size_t end;
+};
 
 extern void*
 re_create(char const*);
@@ -6,10 +14,84 @@ re_create(char const*);
 extern void
 re_destroy(void*);
 
+extern int
+re_match(void*, char const*);
+
+extern struct find_match
+re_find(void*, char const*);
+
+extern void*
+re_captures(void*, char const*);
+
+extern void
+re_captures_destroy(void*);
+
+extern struct find_match
+re_capture_get(void*, size_t);
+
+extern void*
+re_find_iter(void*, char const*);
+
+extern void
+re_destroy_find_iter(void*);
+
+extern struct find_match
+re_find_iter_next(void*);
+
+extern void*
+re_captures_iter(void*, char const*);
+
+extern void
+re_destroy_captures_iter(void*);
+
+extern void*
+re_captures_iter_next(void*);
+
+void
+print_found(struct find_match found, char const* in)
+{
+    if (!found.found)
+    {
+        puts("<not found>");
+        return;
+    }
+
+    printf(
+        "start: %llu, end: %llu, string: ",
+        (unsigned long long)found.start,
+        (unsigned long long)found.end
+    );
+
+    for (size_t i = found.start; i != found.end; ++i)
+    {
+        putchar(in[i]);
+    }
+
+    puts("");
+}
+
 int
 main()
 {
-    void* re = re_create("^h*");
-    puts("Hello world!");
+    void* re = re_create("^[hH]+");
+
+    printf("%d, %d, %d\n", re_match(re, "hhh"), re_match(re, "jjj"), re_match(re, "H"));
+
+    char const* str1 = "hhhlll";
+    char const* str2 = "Hlll";
+    char const* str3 = "lll";
+
+    print_found(re_find(re, str1), str1);
+    print_found(re_find(re, str2), str2);
+    print_found(re_find(re, str3), str3);
+
+    void* cap = re_captures(re, str1);
+
+    if (cap)
+    {
+        print_found(re_capture_get(cap, 0), str1);
+        re_captures_destroy(cap);
+    }
+
     re_destroy(re);
 }
--- rs-re/src/lib.rs
+++ rs-re/src/lib.rs
@@ -1,10 +1,24 @@
-use std::ffi::{c_char, c_void, CStr};
+use regex::{Captures, Regex};
+use std::ffi::{c_char, c_int, c_void, CStr};
+
+#[allow(non_camel_case_types)]
+#[repr(C)]
+pub struct find_match {
+    pub found: c_int,
+    pub start: usize,
+    pub end: usize,
+}
 
 #[no_mangle]
 pub extern "C" fn re_create(c_str: *const c_char) -> *mut c_void {
     unsafe {
         Box::into_raw(Box::new(
-            regex::Regex::new(CStr::from_ptr(c_str).to_str().unwrap()).unwrap(),
+            Regex::new(
+                CStr::from_ptr(c_str)
+                    .to_str()
+                    .expect("regex could not become a string slice"),
+            )
+            .expect("regex could not be compiled"),
         ))
         .cast()
     }
@@ -12,7 +26,125 @@ pub extern "C" fn re_create(c_str: *const c_char) -> *mut c_void {
 
 #[no_mangle]
 pub extern "C" fn re_destroy(v_ptr: *mut c_void) {
-    let re = unsafe { Box::from_raw(v_ptr.cast::<regex::Regex>()) };
+    let re = unsafe { Box::from_raw(v_ptr.cast::<Regex>()) };
 
-    println!("{}", re.as_str());
+    println!("destroying regex: /{}/", re.as_str());
+}
+
+#[no_mangle]
+pub extern "C" fn re_match(v_ptr: *mut c_void, content: *const c_char) -> c_int {
+    let re: &Regex = unsafe { v_ptr.cast::<Regex>().as_ref().expect("regex was null") };
+
+    let content: &str = unsafe {
+        CStr::from_ptr(content)
+            .to_str()
+            .expect("content couldn not become a string slice")
+    };
+
+    if re.is_match(content) {
+        1
+    } else {
+        0
+    }
+}
+#[no_mangle]
+pub extern "C" fn re_find(v_ptr: *mut c_void, content: *const c_char) -> find_match {
+    let re: &Regex = unsafe { v_ptr.cast::<Regex>().as_ref().expect("regex was null") };
+
+    let content: &str = unsafe {
+        CStr::from_ptr(content)
+            .to_str()
+            .expect("content could not become a string slice")
+    };
+
+    let Some(m) = re.find(content) else {
+        return find_match {found: 0, start:0, end:0};
+    };
+
+    find_match {
+        found: 1,
+        start: m.start(),
+        end: m.end(),
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn re_captures(v_ptr: *mut c_void, content: *const c_char) -> *mut c_void {
+    let re: &Regex = unsafe { v_ptr.cast::<Regex>().as_ref().expect("regex was null") };
+
+    let content: &str = unsafe {
+        CStr::from_ptr(content)
+            .to_str()
+            .expect("content couldn not become a string slice")
+    };
+
+    if let Some(caps) = re.captures(content) {
+        Box::into_raw(Box::new(caps)).cast()
+    } else {
+        std::ptr::null_mut()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn re_captures_destroy(v_ptr: *mut c_void) {
+    if v_ptr.is_null() {
+        return;
+    }
+    let _ = unsafe { Box::from_raw(v_ptr.cast::<Captures>()) };
+
+    println!("destroying captures");
+}
+
+#[no_mangle]
+pub extern "C" fn re_capture_get(v_ptr: *mut c_void, number: usize) -> find_match {
+    if let Some(ref m) = unsafe {
+        v_ptr
+            .cast::<Captures>()
+            .as_ref()
+            .expect("could not turn captures to ref")
+    }
+    .get(number)
+    {
+        find_match {
+            found: 1,
+            start: m.start(),
+            end: m.end(),
+        }
+    } else {
+        find_match {
+            found: 0,
+            start: 0,
+            end: 0,
+        }
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn re_find_iter(v_ptr: *mut c_void, content: *const c_char) -> *mut c_void {
+    std::unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn re_destroy_find_iter(v_ptr: *mut c_void) {
+    std::unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn re_find_iter_next(v_ptr: *mut c_void) -> find_match {
+    std::unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn re_captures_iter(v_ptr: *mut c_void) {
+    std::unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn re_destroy_captures_iter(v_ptr: *mut c_void) {
+    std::unimplemented!()
+}
+
+#[no_mangle]
+pub extern "C" fn re_captures_iter_next(v_ptr: *mut c_void) -> *mut c_void {
+    std::unimplemented!()
 }
